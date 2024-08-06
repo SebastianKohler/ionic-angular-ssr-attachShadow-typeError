8a83b1ca0904d68cd51d3960ae4ad5b9bb71eaae
Sebastian Köhler <74658813+SebastianKohler@users.noreply.github.com>
2024-08-06 11:56:59 +0300

    switch to webpack-based `browser` angular builder

--- server.ts
+++ server.ts
@@ -1,16 +1,18 @@
+import 'zone.js/node';
 import { APP_BASE_HREF } from '@angular/common';
 import { CommonEngine } from '@angular/ssr';
 import express from 'express';
-import { fileURLToPath } from 'node:url';
-import { dirname, join, resolve } from 'node:path';
+import { join } from 'node:path';
+import { existsSync } from 'fs';
 import AppServerModule from './src/main.server';
 
 // The Express app is exported so that it can be used by serverless Functions.
 export function app(): express.Express {
   const server = express();
-  const serverDistFolder = dirname(fileURLToPath(import.meta.url));
-  const browserDistFolder = resolve(serverDistFolder, '../browser');
-  const indexHtml = join(serverDistFolder, 'index.server.html');
+  const browserDistFolder = join(process.cwd(), './dist/app/browser');
+  const indexHtml = existsSync(join(browserDistFolder, 'index.original.html'))
+    ? join(browserDistFolder, 'index.original.html')
+    : join(browserDistFolder, 'index.html');
 
   const commonEngine = new CommonEngine();
 
@@ -20,13 +22,13 @@ export function app(): express.Express {
   // Example Express Rest API endpoints
   // server.get('/api/**', (req, res) => { });
   // Serve static files from /browser
-  server.get('**', express.static(browserDistFolder, {
+  server.get('*.*', express.static(browserDistFolder, {
     maxAge: '1y',
     index: 'index.html',
   }));
 
   // All regular routes use the Angular engine
-  server.get('**', (req, res, next) => {
+  server.get('*', (req, res, next) => {
     const { protocol, originalUrl, baseUrl, headers } = req;
 
     commonEngine
